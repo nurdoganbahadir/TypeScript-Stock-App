9811e764ad3cdd1a261fb7bc8e8fe3c3bff9f6a3
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-22 15:35:31 +0300

    navbar ve footer eklendi route yapısı yeniden düzenlendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import store, { persistor } from "./app/store";
 import AppRouter from "./router/AppRouter";
 import React from "react";
@@ -8,7 +9,9 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AppRouter />
+        <Router>
+          <AppRouter />
+        </Router>
       </PersistGate>
     </Provider>
   );
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
 import PrivateRouter from "./PrivateRouter";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const AppRouter: React.FC = () => {
+  const location = useLocation();
+  const hideNavbarAndFooter =
+    location.pathname === "/" || location.pathname === "/register";
+
   return (
-    <Router>
+    <>
+      {!hideNavbarAndFooter && <Navbar />}
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
@@ -15,7 +22,8 @@ const AppRouter: React.FC = () => {
           <Route path="/home" element={<Home />} />
         </Route>
       </Routes>
-    </Router>
+      {!hideNavbarAndFooter && <Footer />}
+    </>
   );
 };
 
